415bf78aefe3e1c4fc4880da9eab03661fc27185
Justin Hui <huirayj@gmail.com>
2021-06-25 14:04:48 -0400

    created seeds

---
+++ seeds/user-seed.js
@@ -0,0 +1,19 @@
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: 'User1',
+        email: 'user1@gmail.com',
+        password: 'password123'
+    },
+    {
+        username: 'User2',
+        email: 'user2@gmail.com',
+        password: 'password123'
+    },
+    {
+        username: 'User3',
+        email: 'user3@gmail.com',
+        password: 'password123'
+    }
+]
