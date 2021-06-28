4945dfcfb53878e4e1440978fcd990e9d49ef203
Justin Hui <huirayj@gmail.com>
2021-06-28 10:46:18 -0400

    commented out unused routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,14 +9,15 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.get('/signup', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('signup');
-});
+// router.get('/signup', (req, res) => {
+//     if (req.session.loggedIn) {
+//         res.redirect('/');
+//         return;
+//     }
+//     res.render('signup');
+// });
 
+<<<<<<< Updated upstream
 router.get('/', async (req, res) => {
     try {
         const allPostData = await Post.findAll({
@@ -45,31 +46,63 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+=======
+// router.get('/', async (req, res) => {
+//     try {
+//         const allPostData = await Post.findAll({
+//             attributes: ['id', 'title', 'content', 'created_at'],
+//             include: [
+//                 {
+//                 model: Comment,
+//                 attributes: ['id', 'content', 'post_id', 'user_id', 'created_at'],
+//                 include: {
+//                     model: User,
+//                     attributes: ['username']
+//                 }
+//             },
+//             {
+//                 model: User,
+//                 attributes: ['username']
+//             }
+//         ]
+//         })
+//         const posts = allPostData.map(post => post.get({ plain: true }));
+
+//         res.render('homepage', {
+//             posts,
+//             loggedIn: req.session.loggedIn
+//         });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+>>>>>>> Stashed changes
 
 
-router.get('/post/:id', async (req, res) => {
-    try {
-        const aPostData = await Post.findByPk(req.params.id, {
-            attributes: ['id', 'title', 'content', 'created_at'],
-            include: [{
-                model: Comment,
-                attributes: ['id', 'content', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }]
-        });
+// router.get('/post/:id', async (req, res) => {
+//     try {
+//         const aPostData = await Post.findByPk(req.params.id, {
+//             attributes: ['id', 'title', 'content', 'created_at'],
+//             include: [{
+//                 model: Comment,
+//                 attributes: ['id', 'content', 'post_id', 'user_id', 'created_at'],
+//                 include: {
+//                     model: User,
+//                     attributes: ['username']
+//                 }
+//             },
+//             {
+//                 model: User,
+//                 attributes: ['username']
+//             }]
+//         });
     
-        if (!aPostData) {
-            res.status(404).json({ message: 'No post found with that id' });
-        } else {
-            const post = aPostData.get({ plain: true });
+//         if (!aPostData) {
+//             res.status(404).json({ message: 'No post found with that id' });
+//         } else {
+//             const post = aPostData.get({ plain: true });
     
+<<<<<<< Updated upstream
             res.status(200).render('single-post', {
                 post,
                 loggedIn: req.session.loggedIn
@@ -79,5 +112,16 @@ router.get('/post/:id', async (req, res) => {
         res.status(500).json(err);
     }
 });
+=======
+//             res.render('single-post', {
+//                 post,
+//                 loggedIn: req.session.loggedIn
+//             });
+//         }
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+>>>>>>> Stashed changes
 
 module.exports = router;
