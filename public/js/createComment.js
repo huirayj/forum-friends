58e1aeb7136c1b3727d22d4a2a920fd1c8fedfe6
Justin Hui <huirayj@gmail.com>
2021-06-29 18:36:59 -0400

    changed logout to class

--- public/js/createComment.js
+++ public/js/createComment.js
@@ -1,28 +1,27 @@
 const commentFormHandler = async (e) => {
-    e.preventDefault();
-  
-    const comment = document.querySelector('textarea[name="comment-body"]').value.trim();
-    const location = window.location.toString().split('/');
-    const post_id = location[location.length - 1];
-  
-    if (comment) {
-        const res = await fetch('/api/comments', {
-          method: 'POST',
-          body: JSON.stringify({
-            post_id,
-            comment
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-      
-        if (res.ok) {
-          document.location.reload();
-        } else {
-          alert(res.statusText);
-        }
+  e.preventDefault();
+
+  const content = document.querySelector('#textarea1').value.trim();
+  const post_id = document.querySelector('.comment-btn').getAttribute('data-id');
+
+  if (content) {
+    const res = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        post_id,
+        content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
       }
-  };
-  
-  document.querySelector('.new-comment-form').addEventListener('submit', commentFormHandler);
+    });
+
+    if (res.ok) {
+      document.location.reload();
+    } else {
+      alert(res.statusText);
+    }
+  }
+};
+
+document.querySelector('.new-comment-form').addEventListener('submit', commentFormHandler);
